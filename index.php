7b621e1078e1c2ef16d3f7868ad745a80529e393
Larry Latorre <llatorre481@gmail.com>
2015-07-14 20:13:56 -0600

    created amazon-search persona and three use cases

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Data Design</title>
+	</head>
+	<body>
+		<p><h3>Persona</h3></p>
+		<p>My target audience is anyone and everyone looking to buy something. We are Amazon and have
+		almost anything you can think for sale.</p>
+		<p><h3>Use Cases</h3></p>
+		<p>
+			<ol>
+				<li>User logs in and searches for products by typing in a specific item and then hits submit button.</li>
+				<li>Search results return product data(price, quantity, reviews, etc.) for user.</li>
+				<li>User can then select the item from search results by clicking on the item name.</li>
+			</ol>
+		</p>
+
+	</body>
+</html>
+
+
